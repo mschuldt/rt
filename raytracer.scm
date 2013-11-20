619d4541c187ec1416b2cea080a7bb9010d34865
Andrey Elenskiy <andrey.elenskiy@gmail.com>
2013-11-20 02:54:05 -0800

    Raytracer works...

--- raytracer.scm
+++ raytracer.scm
@@ -1,4 +1,4 @@
-(define canv-width 20)
+(define canv-width 500)
 ;; Sphere: radius (cx  cy  cz) R  G  B specular_exponent reflectiveness
 (define spheres (list
 	canv-width (list 0 (- canv-width) 0)  9 9 0  canv-width  2  ;; Yellow sphere
@@ -6,9 +6,11 @@
 	1 (list (- 2)  1 4)  0 9 0  9  4  ;; Green sphere
 	1 (list 2  1 4)  0 0 9  canv-width  5   ;; Blue sphere
 	))
+(define spheres-length (length spheres)) ;some optimization
 
 (define ambient-light 2)
 (define lights (list 8 (list 2 2 0))) ;; always even size
+(define lights-length (length lights)) ;some optimization
 (define (set-dot x y r g b) (setpos x y) (dot 1 r g b))
 (penup)
 
@@ -16,8 +18,6 @@
   (car (cdr x)))
 (define (caddr x)
   (car (cdr (cdr x))))
-(define (cadddr x)
-  (car (cdr (cdr (cdr x)))))
 
 (define (get-by-index alist start index list-length)
 		(cond
@@ -26,10 +26,10 @@
 			(else (get-by-index (cdr alist) (+ start 1) index list-length))))
 
 (define (spheres-by-index index)
-	(get-by-index spheres 0 index (length spheres)))
+	(get-by-index spheres 0 index spheres-length))
 
 (define (lights-by-index index)
-	(get-by-index lights 0 index (length lights)))
+	(get-by-index lights 0 index lights-length))
 
 (define (max a b)
 	(if (>= a b) a b))
@@ -61,10 +61,6 @@
   (< (abs (- (square guess) x)) 0.001))
 
 (define (sqrt x)
-  (define (good-enough? guess)
-    (< (abs (- (square guess) x)) 0.001))
-  (define (improve guess)
-    (average guess (/ x guess)))
   (define (sqrt-iter guess)
     (if (good-enough? guess)
         guess
@@ -85,7 +81,6 @@
 
 
 (define closest-intersection (mu (source direction t_min t_max)
-	(define min-dist canv-width)
 	(define (closest-sphere-index v q)
 		(cond
 			((null? (spheres-by-index q)) v)
@@ -96,22 +91,23 @@
 				(define a (* 2 (dot-product direction direction)))
 				(define b (- (* 2 (dot-product j direction))))
 				(define discr (- (square b) (* 2 a (- (dot-product j j) (square radius)))))
-				(if (>= discr 0) (begin
+				(if (> discr 0) (begin
 					(define discr (sqrt discr))
 					(define sol1 (/ (- b discr) a))
 					(if (and (< t_min sol1) (< sol1 t_max) (< sol1 min-dist))(begin
-						(set! v q)
-						(set! min-dist sol1)))
+						(define v q)
+						(set! min-dist sol1))) ; thanks to mu
 					(define sol2 (/ (- b (- discr)) a))
 					(if (and (< t_min sol2) (< sol2 t_max) (< sol2 min-dist))(begin
-						(set! v q)
-						(set! min-dist sol2)))
+						(define v q)
+						(set! min-dist sol2))) ; thanks to mu
 					))
 				(closest-sphere-index v (+ q 6)))))
 	(closest-sphere-index 0 0)))
 
 
 (define trace-ray (mu (source direction t_min t_max depth) ; MU Procedure I LOVE YOU <3
+	(define min-dist canv-width)
 	(define closest-sphere (closest-intersection source direction t_min t_max))
 	(if (= 0 closest-sphere) 0 (begin
 		(define intersection (a-minus-bk source direction (- min-dist)))
@@ -128,7 +124,7 @@
 					(define m (a-minus-bk light-vector normal (/ (* 2 k) n)))
 					(define illumination (+ illumination
 						(*
-							(closest-intersection intersection light-vector (/ 1 canv-width) 1)
+							(if (= 0 (closest-intersection intersection light-vector (/ 1 canv-width) 1)) 1 0)
 							intensity
 							(+
 								(max 0 (/ k (sqrt (* (dot-product light-vector light-vector) n))))
@@ -144,15 +140,16 @@
 			2.8))
 		(define reflection (/ (spheres-by-index (+ closest-sphere 5)) 9))
 		(if (> depth 0)
-			(+ (*
-				(trace-ray
-					intersection
-					(a-minus-bk direction normal (* 2 (/ (dot-product normal direction) n)))
-					(/ 1 canv-width)
-					canv-width
-					(- depth 1))
-				reflection)
-			(* local-color (- 1 reflection)))
+			(+
+				(*
+					(trace-ray
+						intersection
+						(a-minus-bk direction normal (/ (* 2 (dot-product normal direction)) n))
+						(/ 1 canv-width)
+						canv-width
+						(- depth 1))
+					reflection)
+				(* local-color (- 1 reflection)))
 			local-color)))))
 
 (define half (/ canv-width 2))
@@ -165,28 +162,15 @@
 (define draw-x (mu (x-left x-right)
 	(if (< x-left x-right)
 		(begin
+			(define camera (list 0 1 0))
+			(define direction (list (/ x-left canv-width) (/ y-top canv-width) 1))
 			(define color-channel 1)
-			(define r (trace-ray ;; thank you mu!
-				(list 0 1 0)
-				(list (/ x-left canv-width) (/ y-top canv-width) 1)
-				1
-				canv-width
-				2))
+			(define r (trace-ray camera direction 1 canv-width 2)) ;; thank you mu!
 			(define color-channel 2)
-			(define g (trace-ray ;; thank you mu!
-				(list 0 1 0)
-				(list (/ x-left canv-width) (/ y-top canv-width) 1)
-				1
-				canv-width
-				2))
+			(define g (trace-ray camera direction 1 canv-width 2)) ;; thank you mu!
 			(define color-channel 3)
-			(define b (trace-ray ;; thank you mu!
-				(list 0 1 0)
-				(list (/ x-left canv-width) (/ y-top canv-width) 1)
-				1
-				canv-width
-				2))
-			(set-dot x-left y-top r g b)
+			(define b (trace-ray camera direction 1 canv-width 2)) ;; thank you mu!
+			(set-dot x-left y-top (if (> r 255) 255 r) (if (> g 255) 255 g) (if (> b 255) 255 b))
 			(draw-x (+ x-left 1) x-right)))))
 
 (draw-y half (- half))
